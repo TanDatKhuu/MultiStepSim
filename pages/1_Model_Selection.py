5e9222a7c84670cd99ff7849478ddaf5e2f4c513
Tan Dat Khuu <tandatkhuu2k3@gmail.com>
2025-07-28 12:14:17 +0700

    Update 1_Model_Selection.py

--- pages/1_Model_Selection.py
+++ pages/1_Model_Selection.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import streamlit as st
 from modules.models import MODELS_DATA
-import os
+
 
 # Bắt buộc phải có để Streamlit biết cách cấu hình trang
 st.set_page_config(page_title="Chọn Mô Hình", layout="wide")
