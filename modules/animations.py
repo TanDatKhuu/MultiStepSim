f561a3c91b7a37e9b394679898883882b33c7bef
Tan Dat Khuu <tandatkhuu2k3@gmail.com>
2025-07-28 10:43:21 +0700

    Update animations.py

--- modules/animations.py
+++ modules/animations.py
@@ -1,1 +1,166 @@
+import numpy as np
+import random
+from matplotlib.patches import Circle
 
+
+# --- Các hằng số cần cho model 3
+AGENT_DIRECTION_CHANGE_PROB = 0.02
+AGENT_MAX_ANGLE_PERTURBATION_DEG = 10
+MAX_TOTAL_AGENTS_FOR_FULL_DISPLAY = 150 
+SAMPLE_SIZE_FOR_LARGE_POPULATION = 100 
+ABM_ROOM_DIMENSION_DEFAULT = 10.0
+ABM_AGENT_SPEED_DEFAULT = 0.05
+ABM_CONTACT_RADIUS_DEFAULT = 0.55
+ABM_R_FACTOR_DEFAULT = 1000
+ABM_PTRANS_MIN = 0.01
+ABM_PTRANS_MAX = 0.9
+ABM_MAX_STEPS_DEFAULT = 400
+ABM_INTERVAL_DEFAULT = 120
+# --- Class Mô phỏng model 2 ---
+class Cell:
+    def __init__(self, x, y, gen=0):
+        self.x = x
+        self.y = y
+        self.gen = gen
+        self.last_division = -100
+
+# --- Class Mô phỏng model 3 ---
+class DiseaseSimulationABM:
+    def __init__(self, total_population, initial_infected_count_for_abm, room_dimension,
+                 contact_radius, transmission_prob, agent_speed):
+        self.n_total_population_initial = total_population
+        self.n_total_population = total_population
+        self.n_infected_initial_abm = initial_infected_count_for_abm
+        self.room_dimension = room_dimension
+        self.contact_radius = contact_radius
+        self.transmission_prob = transmission_prob
+        self.agent_speed = agent_speed
+        self.susceptible_coords = np.zeros((0,2))
+        self.infected_coords = np.zeros((0,2))
+        self.susceptible_velocities = np.zeros((0,2))
+        self.infected_velocities = np.zeros((0,2))
+        self.current_time_step = 0
+        self.contact_radius_patches = []
+        self._initialize_agents()
+
+    def _initialize_agents(self):
+        if self.n_infected_initial_abm > self.n_total_population:
+            raise ValueError("Số nhiễm ban đầu không thể lớn hơn tổng dân số.")
+        all_coords = np.random.rand(self.n_total_population, 2) * self.room_dimension
+        actual_initial_infected = min(self.n_infected_initial_abm, self.n_total_population)
+        if actual_initial_infected > self.n_total_population:
+            actual_initial_infected = self.n_total_population      
+        if self.n_total_population > 0 and actual_initial_infected > 0 :
+             infected_indices = np.random.choice(self.n_total_population, actual_initial_infected, replace=False)
+        elif actual_initial_infected == 0:
+             infected_indices = np.array([], dtype=int)
+        else: 
+             infected_indices = np.array([], dtype=int)
+        susceptible_mask = np.ones(self.n_total_population, dtype=bool)
+        if len(infected_indices) > 0:
+            susceptible_mask[infected_indices] = False       
+        self.infected_coords = all_coords[infected_indices]
+        self.susceptible_coords = all_coords[susceptible_mask]
+        num_susceptible = self.susceptible_coords.shape[0]
+        if num_susceptible > 0:
+            angles_s = np.random.rand(num_susceptible) * 2 * np.pi
+            self.susceptible_velocities = self.agent_speed * np.array([np.cos(angles_s), np.sin(angles_s)]).T
+        else:
+            self.susceptible_velocities = np.zeros((0,2))
+        num_infected = self.infected_coords.shape[0]
+        if num_infected > 0:
+            angles_i = np.random.rand(num_infected) * 2 * np.pi
+            self.infected_velocities = self.agent_speed * np.array([np.cos(angles_i), np.sin(angles_i)]).T
+        else:
+            self.infected_velocities = np.zeros((0,2))
+
+    def _move_agents(self, coords_array, velocities_array):
+        if coords_array.shape[0] == 0: return coords_array, velocities_array
+        coords_array += velocities_array
+        for i in range(coords_array.shape[0]):
+            for dim in range(2):
+                if coords_array[i, dim] < 0:
+                    coords_array[i, dim] = 0
+                    velocities_array[i, dim] *= -1
+                elif coords_array[i, dim] > self.room_dimension:
+                    coords_array[i, dim] = self.room_dimension
+                    velocities_array[i, dim] *= -1
+        for i in range(velocities_array.shape[0]):
+            if np.random.rand() < AGENT_DIRECTION_CHANGE_PROB:
+                angle_perturbation_rad = np.deg2rad((np.random.rand() - 0.5) * 2 * AGENT_MAX_ANGLE_PERTURBATION_DEG)
+                vx, vy = velocities_array[i, 0], velocities_array[i, 1]
+                current_speed_sq = vx**2 + vy**2
+                if current_speed_sq < 1e-12 :
+                    new_angle = np.random.rand() * 2 * np.pi; speed_to_use = self.agent_speed
+                else:
+                    current_angle = np.arctan2(vy, vx); new_angle = current_angle + angle_perturbation_rad; speed_to_use = self.agent_speed
+                velocities_array[i, 0] = speed_to_use * np.cos(new_angle)
+                velocities_array[i, 1] = speed_to_use * np.sin(new_angle)
+        return coords_array, velocities_array
+
+    def _check_infections(self):
+        if self.susceptible_coords.shape[0] == 0 or self.infected_coords.shape[0] == 0: return
+        newly_infected_indices = []
+        for i, s_pos in enumerate(self.susceptible_coords):
+            if self.infected_coords.shape[0] > 0:
+                distances_sq = np.sum((self.infected_coords - s_pos)**2, axis=1)
+                min_dist_sq_to_infected = np.min(distances_sq)
+                if min_dist_sq_to_infected < self.contact_radius**2:
+                    if np.random.rand() < self.transmission_prob: newly_infected_indices.append(i)
+        if newly_infected_indices:
+            newly_infected_indices_np = np.array(sorted(list(set(newly_infected_indices)), reverse=True))
+            agents_coords_to_move = self.susceptible_coords[newly_infected_indices_np]
+            agents_velocities_to_move = self.susceptible_velocities[newly_infected_indices_np]
+            if agents_coords_to_move.shape[0] > 0:
+                 self.infected_coords = np.vstack((self.infected_coords, agents_coords_to_move))
+                 self.infected_velocities = np.vstack((self.infected_velocities, agents_velocities_to_move))
+            self.susceptible_coords = np.delete(self.susceptible_coords, newly_infected_indices_np, axis=0)
+            self.susceptible_velocities = np.delete(self.susceptible_velocities, newly_infected_indices_np, axis=0)
+
+    def step(self):
+        self.current_time_step += 1
+        self.susceptible_coords, self.susceptible_velocities = self._move_agents(self.susceptible_coords, self.susceptible_velocities)
+        self.infected_coords, self.infected_velocities = self._move_agents(self.infected_coords, self.infected_velocities)
+        self._check_infections()
+        current_total_pop = len(self.susceptible_coords) + len(self.infected_coords)
+        if abs(current_total_pop - self.n_total_population_initial) > 0 and self.current_time_step > 1 : 
+             pass
+        num_susceptible = len(self.susceptible_coords); num_infected = len(self.infected_coords)
+        if num_susceptible == 0 and self.n_total_population_initial > 0:
+            print(f"Tất cả {self.n_total_population_initial} người đã bị nhiễm tại bước thời gian {self.current_time_step}.")
+            return True
+        return False
+
+    def update_contact_circles(self, ax, coords_to_display):
+        for patch in self.contact_radius_patches: patch.remove()
+        self.contact_radius_patches.clear()
+        for i_pos in coords_to_display:
+            circle = Circle((i_pos[0], i_pos[1]), self.contact_radius, edgecolor='red', facecolor='none', alpha=0.2, linewidth=0.8, linestyle='--'); ax.add_patch(circle); self.contact_radius_patches.append(circle)
+
+    def get_current_stats(self):
+        return {
+            "time_step": self.current_time_step,
+            "susceptible_count": len(self.susceptible_coords),
+            "infected_count": len(self.infected_coords),
+            "total_population": self.n_total_population_initial
+        }
+
+# --- Hàm helper cho hiển thị model 3 ---
+def get_display_coords_mixed(s_coords, i_coords, max_total_full, sample_size_large):
+    total_current_agents = s_coords.shape[0] + i_coords.shape[0] 
+    if total_current_agents <= max_total_full:
+        return s_coords, i_coords
+    else:
+        s_display = s_coords
+        i_display = i_coords
+        ratio_s = s_coords.shape[0] / total_current_agents if total_current_agents > 0 else 0
+        ratio_i = i_coords.shape[0] / total_current_agents if total_current_agents > 0 else 0
+        num_s_to_sample = int(sample_size_large * ratio_s)
+        num_i_to_sample = sample_size_large - num_s_to_sample 
+        if s_coords.shape[0] > num_s_to_sample:
+            s_indices = np.random.choice(s_coords.shape[0], num_s_to_sample, replace=False)
+            s_display = s_coords[s_indices]     
+        if i_coords.shape[0] > num_i_to_sample:
+            i_indices = np.random.choice(i_coords.shape[0], num_i_to_sample, replace=False)
+            i_display = i_coords[i_indices]          
+        return s_display, i_display
