ceb728f76ce17dff2fe63f08cebd3d4c1207b1b2
Tan Dat Khuu <tandatkhuu2k3@gmail.com>
2025-07-28 12:15:17 +0700

    Update 3_Dynamic_Simulation.py

--- pages/3_Dynamic_Simulation.py
+++ pages/3_Dynamic_Simulation.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import streamlit as st
 import time
 import numpy as np
